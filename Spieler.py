06c3b91b881577d8c164033c5e5f30db720826f1
MostafaGanji <88392156+MostafaGanji@users.noreply.github.com>
2023-05-11 23:38:08 +0200

    Update Spieler.py

--- Spieler.py
+++ Spieler.py
@@ -1,21 +1,29 @@
 import pygame
-from Spielfeld import *
+import Spielfeld
 
-class Spieler1():
+pygame.init()
+clock = pygame.time.Clock()
 
-    pygame.init()
 
-    SpielerIMG = pygame.image.load(r"C:\Users\LENOVO\Desktop\spacesurvivor\Bilder\\space.png")
+
+lost_font_text = pygame.font.SysFont('arial',200)
+live = pygame.font.SysFont('Verdana',400)
+
+
+x = (Spielfeld.Spielfeld_width * 0.45)
+y = (Spielfeld.Spielfeld_height * 0.8)
+
+
+class Spieler():
+    SpielerIMG = pygame.image.load(r"C:\Users\Mostafa Ganji\Desktop\Spiel_projekt\\space.png")
     SpielerIMG = pygame.transform.scale(SpielerIMG, (40, 40))
 
 
-
     def Spaceship(x,y):
-     Spielfeld.Spielfeld.blit(SpielerIMG, (x,y))
+     Spielfeld.blit(Spieler.SpielerIMG, (x,y))
 
 
-     x = (Spielfeld.Spielfeld_width * 0.45)
-     y = (Spielfeld.Spielfeld_height * 0.8)
+
 
     def move():
         for event in pygame.event.get():
@@ -42,8 +50,8 @@ class Spieler1():
 
 
         Spielfeld.clock.tick(60)
-        Spielfeld.fill(Spielfeld.white)
-        SpielerIMG(x,y)
+        
+        Spieler.SpielerIMG(x,y)
         x += x_change 
 
 
@@ -52,18 +60,16 @@ class Spieler1():
     Leben = 3
 
     
-
     
-    lost_font_text = pygame.font.Font('Verdana',200)
     def lost_text():
-        over_text = lost_font_text.render("You have lost", True, (255, 255, 255))
-        screen.blit(over_text, (400, 400)) 
+        over_text = lost_font_text.render("you lost", True, (255, 255, 255))
+        Spielfeld.blit(over_text, (400, 400)) 
 
     
     
     def lives(x, y):
-        live = font.render("Leben : " + str(Leben), True, (255, 255, 255))
-        screen.blit(live, (Spielfeld.Spielfeld_width -100, Spielfeld.Spielfeld_height))    
+        live = live.render("Leben : ", True, (255, 255, 255))
+        Spielfeld.blit(live, (Spielfeld.Spielfeld_width -100, Spielfeld.Spielfeld_height))    
     
     
     
@@ -74,4 +80,4 @@ class Spieler1():
     
     
     
-    pygame.display.update()    
+    
