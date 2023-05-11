0bc3e5914ae4bae33eacefb34dd0f4c70be4b7dc
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-05-11 21:39:52 +0200

    Fertig!

--- leaderboard.py
+++ leaderboard.py
@@ -1,15 +1,22 @@
 class leaderboard:
-	spieler=["Gerald","Alf","Hans","Ralf","Kai","Harry"]
-	punktzahl=[5,7,1,3,4,6]
+	spieler=[]
+	punktzahl=[]
 
 	def addToBoard(pSpieler, pPunktzahl):
 		leaderboard.spieler+=pSpieler
 		leaderboard.punktzahl+=pPunktzahl
-	def sortBoardByScore():
+  
+	def sortBoardByScoreDesc():
 		for i in range(len(leaderboard.punktzahl)):
 			for j in range(len(leaderboard.punktzahl)-1-i):
 				if leaderboard.punktzahl[j] > leaderboard.punktzahl[j + 1]:
 						leaderboard.punktzahl[j], leaderboard.punktzahl[j+1],leaderboard.spieler[j], leaderboard.spieler[j+1] = leaderboard.punktzahl[j+1], leaderboard.punktzahl[j],leaderboard.spieler[j+1], leaderboard.spieler[j]
+      
+	def deleteUnnecessaryEntries():
+		while len(leaderboard.punktzahl)>10:
+			leaderboard.punktzahl, leaderboard.spieler=leaderboard.punktzahl[1:], leaderboard.spieler[1:]
 
-
-			
+	def updateBoard(pSpieler, pPunktzahl):
+		leaderboard.addToBoard(pSpieler, pPunktzahl)
+		leaderboard.sortBoardByScoreDesc()
+		leaderboard.deleteUnnecessaryEntries()
