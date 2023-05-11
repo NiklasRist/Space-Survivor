5c9c693b0b16969963f9adba6b8f8b80426f1188
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-05-11 23:01:09 +0200

    Fortschritt

--- speicher.py
+++ speicher.py
@@ -1,4 +1,9 @@
 #Hilfestellung
-#https://www.python-kurs.eu/sql_python.php
+#https://docs.python.org/3/library/sqlite3.html#   !!!!besser!!!!
 import sqlite3
 connection = sqlite3.connect("Speicher.db")
+cursor=connection.cursor()
+cursor.execute("""CREATE TABLE leaderboard (Spieler, Punktzahl)""")
+
+def saveLeaderboard(pSpieler, pPunktzahl): #wie kombiniert man sql_command mit variablen vom typ array???
+    cursor.execute("""INSERT INTO leaderboard()""")
