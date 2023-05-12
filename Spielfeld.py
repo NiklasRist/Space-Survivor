06c101b3fda801b9bbc33746b184a857d6eed797
NiklasRist <79253498+NiklasRist@users.noreply.github.com>
2023-05-12 11:09:27 +0200

    Update Spielfeld.py

--- Spielfeld.py
+++ Spielfeld.py
@@ -1,9 +1,11 @@
 import pygame
  
-white = (255,255,255)
-Spielfeld_width = 800
-Spielfeld_height = 800
+
 class Spieldfeld():
-  Spielfeld = pygame.display.set_mode([Spielfeld_width,Spielfeld_height])
-  pygame.display.set_caption("Space survior")
-  Spielfeld.fill(white)
+  
+  Spielfeld_width = 800
+  Spielfeld_height = 800
+  #background
+  
+  
+  #Bild von Background
