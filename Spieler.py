93d8a064b7aec9e2a39798b7b73fbdd8fda43811
NiklasRist <79253498+NiklasRist@users.noreply.github.com>
2023-05-12 11:08:52 +0200

    Update Spieler.py

--- Spieler.py
+++ Spieler.py
@@ -6,23 +6,17 @@ clock = pygame.time.Clock()
 
 
 
-lost_font_text = pygame.font.SysFont('arial',200)
-live = pygame.font.SysFont('Verdana',400)
 
 
-x = (Spielfeld.Spielfeld_width * 0.45)
-y = (Spielfeld.Spielfeld_height * 0.8)
+
 
 
 class Spieler():
     SpielerIMG = pygame.image.load(r"C:\Users\Mostafa Ganji\Desktop\Spiel_projekt\\space.png")
     SpielerIMG = pygame.transform.scale(SpielerIMG, (40, 40))
-
-
-    def Spaceship(x,y):
-     Spielfeld.blit(Spieler.SpielerIMG, (x,y))
-
-
+    x = (Spielfeld.Spielfeld_width * 0.45)
+    y = (Spielfeld.Spielfeld_height * 0.8)
+    Leben = 3
 
 
     def move():
@@ -51,31 +45,20 @@ class Spieler():
 
         Spielfeld.clock.tick(60)
         
-        Spieler.SpielerIMG(x,y)
+        
         x += x_change 
 
 
-
-
-    Leben = 3
-
-    
-    
-    def lost_text():
-        over_text = lost_font_text.render("you lost", True, (255, 255, 255))
-        Spielfeld.blit(over_text, (400, 400)) 
-
-    
-    
-    def lives(x, y):
-        live = live.render("Leben : ", True, (255, 255, 255))
-        Spielfeld.blit(live, (Spielfeld.Spielfeld_width -100, Spielfeld.Spielfeld_height))    
-    
-    
     
 
-    if(Leben <= 0):
-        lost_text()
+        
+    
+    
+    
+    
+#Gehören zur Steuerung:
+    #if(Leben <= 0):
+        #Gui.lost_text()
 
     
     
