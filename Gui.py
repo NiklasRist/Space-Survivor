2e934621b8e1c401fe0617dc33a42740b1f1fc89
NiklasRist <79253498+NiklasRist@users.noreply.github.com>
2023-05-12 11:09:55 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -5,6 +5,30 @@ import sys
 
 pygame.init()
 
+lost_font_text = pygame.font.SysFont('arial',200)
+live = pygame.font.SysFont('Verdana',400)
+
+
+
+def create_Fenster():
+  white = (255,255,255)
+  Spielfeld = pygame.display.set_mode([2*Spielfeld.Spielfeld_width,2*Spielfeld.Spielfeld_height])
+  pygame.display.set_caption("Space survior")
+  Spielfeld.fill(white)
+
+def Creat_spieler(x,y):
+  Spielfeld.blit(Spieler.SpielerIMG, (Spieler.x,Spieler.y))
+
+
+def lost_text():
+  over_text = lost_font_text.render("you lost", True, (255, 255, 255))
+    Spielfeld.blit(over_text, (400, 400)) 
+
+    
+def lives():
+  livee = live.render("Leben : ", True, (255, 255, 255))
+  Spielfeld.blit(livee, (Spielfeld.Spielfeld_width -100, Spielfeld.Spielfeld_height))
+
 
 end = False
 
