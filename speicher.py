5edd0c79bde0abb30089bd08f1b2060f59c13e80
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-05-14 13:18:42 +0200

    Progress...

--- speicher.py
+++ speicher.py
@@ -1,24 +1,26 @@
 #Hilfestellung
 #https://docs.python.org/3/library/sqlite3.html#   !!!!besser!!!!
 import sqlite3
+
+#leaderboard id<10 und in leaderboard soll das leaderboard gespeichert werden
+
+
 class Speicher:
     connection = sqlite3.connect("leaderboard.db")
     cursor=connection.cursor()
     
-    sqlCreateTable="""CREATE TABLE leaderboard (Spieler, Punktzahl, id)"""
-    sqlInsertVar="""INSERT INTO leaderboard(Spieler, Punktzahl, id) VALUES (?, ?, ?)"""
+    sqlCreateTable="""CREATE TABLE leaderboard (Spieler, Punktzahl, id)""" #id automatisch zugewiesen, beginnt bei 0
+    sqlInsertVar="""INSERT INTO leaderboard(Spieler, Punktzahl) VALUES (?, ?)"""
     sqlSelectAll="""SELECT Spieler, Punktzahl FROM leaderboard"""
     sqlUpdateOneEntry="""UPDATE leaderboard SET Spieler=?, Punktzahl=? WHERE id=?"""
-    
-    row_id=0
 
     def createTable():
         Speicher.cursor.execute(Speicher.sqlCreateTable)
 
     def saveOneEntryInLeaderboard(pSpieler, pPunktzahl): 
-        Speicher.cursor.execute(Speicher.sqlInsertVar, (pSpieler, pPunktzahl, Speicher.row_id))
+        Speicher.cursor.execute(Speicher.sqlInsertVar, (pSpieler, pPunktzahl))#, Speicher.row_id))
         Speicher.connection.commit()
-        row_id+=1
+        #Speicher.row_id+=1
         
     def updateEntry(pSpieler, pPunktzahl, pZeile):
         Speicher.cursor.execute(Speicher.sqlUpdateOneEntry,(pSpieler, pPunktzahl, pZeile))
@@ -36,9 +38,9 @@ class Speicher:
         Speicher.cursor.execute(Speicher.sqlSelectAll)
         return Speicher.cursor.fetchall() #2-dimensionaler Array [(spieler, punktzahl),(spieler, punktzahl),...]
 
-Speicher.createTable
+#Speicher.createTable
 #Speicher.saveOneEntryInLeaderboard("Nur", 7)
 #Speicher.saveOneEntryInLeaderboard("Nur", 7)
 #Speicher.updateEntry("HArald", 24, 0)
-#print(Speicher.loadEntries)
+#print(Speicher.loadEntries())
  
