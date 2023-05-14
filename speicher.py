cf7880131e103f3f34960009fdef432c8a566825
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-05-14 12:51:13 +0200

    trying...

--- speicher.py
+++ speicher.py
@@ -1,31 +1,44 @@
 #Hilfestellung
 #https://docs.python.org/3/library/sqlite3.html#   !!!!besser!!!!
 import sqlite3
-connection = sqlite3.connect("Speicher.db")
-cursor=connection.cursor()
-
-sqlCreateTable="""CREATE TABLE leaderboard (Spieler, Punktzahl)"""
-sqlInsertVar="""INSERT INTO leaderboard(Spieler, Punktzahl) VALUES (?, ?)"""
-sqlSelectAll="""SELECT * FROM leaderboard"""
-
-def createTable():
-    cursor.execute(sqlCreateTable)
-
-def saveOneEntryInLeaderboard(pSpieler, pPunktzahl): 
-    cursor.execute(sqlInsertVar, (pSpieler, pPunktzahl))
-    connection.commit()
+class Speicher:
+    connection = sqlite3.connect("Speicher.db")
+    cursor=connection.cursor()
     
-def updateEntries():
-    pass
+    sqlCreateTable="""CREATE TABLE leaderboard (Spieler, Punktzahl, id)"""
+    sqlInsertVar="""INSERT INTO leaderboard(Spieler, Punktzahl, id) VALUES (?, ?, ?)"""
+    sqlSelectAll="""SELECT Spieler, Punktzahl FROM leaderboard"""
+    sqlUpdateOneEntry="""UPDATE leaderboard SET Spieler=?, Punktzahl=? WHERE id=?"""
+    
+    row_id=0
 
-def deleteEntries():
-    pass
+    def createTable():
+        Speicher.cursor.execute(Speicher.sqlCreateTable)
 
-def deleteAllEntries():
-    pass
-def loadEntries():
-    cursor.execute(sqlSelectAll)
-    return cursor.fetchall() #2-dimensionaler Array [(spieler, punktzahl),(spieler, punktzahl),...]
+    def saveOneEntryInLeaderboard(pSpieler, pPunktzahl): 
+        Speicher.cursor.execute(Speicher.sqlInsertVar, (pSpieler, pPunktzahl, Speicher.row_id))
+        Speicher.connection.commit()
+        row_id+=1
+        
+    def updateEntry(pSpieler, pPunktzahl, pZeile):
+        Speicher.cursor.execute(Speicher.sqlUpdateOneEntry,(pSpieler, pPunktzahl, pZeile))
+        
+    def updateEntries():
+        pass
 
+    def deleteEntries():
+        pass
 
+    def deleteAllEntries():
+        pass
+
+    def loadEntries():
+        Speicher.cursor.execute(Speicher.sqlSelectAll)
+        return Speicher.cursor.fetchall() #2-dimensionaler Array [(spieler, punktzahl),(spieler, punktzahl),...]
+
+Speicher.createTable
+Speicher.saveOneEntryInLeaderboard("Nur", 7)
+Speicher.saveOneEntryInLeaderboard("Nur", 7)
+Speicher.updateEntry("HArald", 24, 0)
+print(Speicher.loadEntries)
  
