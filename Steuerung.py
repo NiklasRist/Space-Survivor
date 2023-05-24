c0c04413b4e5751407e795fa42a9c70d437bfb25
NiklasRist <79253498+NiklasRist@users.noreply.github.com>
2023-05-24 10:44:19 +0200

    Add files via upload

--- Steuerung.py
+++ Steuerung.py
@@ -25,10 +25,7 @@ class Steuerung(){
         self.speicher_1 = speicher()
         self.end = false 
         self.Gui_1.create_Fenster()
-        self.Gui_1.create_Spielfeld(Spielfeld_1)
-        self.Gui_1.create_Spielfeld(Spielfeld_2)
-        self.Gui_1.Create_spieler()
-        self.Gui_1.Create_spieler()
+        self.game_mode=0 #0=Main Menu, 1=lokaler Mehrspieler, 2=LAN Mehrspieler, 3=Optionen
 
         main_loop()
 
@@ -43,7 +40,16 @@ class Steuerung(){
                     end = True
                     pygame.quit()
                     sys.exit(0)
-           
+            if game_mode=0:
+                main_Menu()
+            elif game_mode=1:
+                lokaler_Mehrspieler()
+            elif game_mode=2:
+                lan_Mehrspieler()
+            elif game_mode=3:
+                optionen()
+            else:
+                game_mode=0
             
             
             
@@ -69,8 +75,23 @@ class Steuerung(){
             Gui_1.Spiel_fenster.flip()
             clock.tick(60)           
             
-}
+    def main_Menu():
+        game_mode=1
+    
+    def lokaler_Mehrspieler():
+        self.Gui_1.create_Spielfeld(Spielfeld_1)
+        self.Gui_1.create_Spielfeld(Spielfeld_2)
+        self.Gui_1.Create_spieler()
+        self.Gui_1.Create_spieler()
 
+    def lan_Mehrspieler():
+        self.Gui_1.create_Spielfeld(Spielfeld_1)
+        self.Gui_1.create_Spielfeld(Spielfeld_2)
+        self.Gui_1.Create_spieler()
+        self.Gui_1.Create_spieler()
+
+    def optionen():
+        pass
     
     
     
