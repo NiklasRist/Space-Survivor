a87b486809f0a351196103afb0948e5a4e24b7c5
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-05-26 15:32:58 +0200

    Progress...

--- npcs.py
+++ npcs.py
@@ -13,9 +13,14 @@ class Gegner:
         self.G_leben = 4
         self.GegnerIMG = pygame.image.load(".\images\TestImage.png")
         self.GegnerIMG = pygame.transform.scale(self.GegnerIMG, (int(0.0375*feld_obj.Spielfeld_width), int(0.0375*feld_obj.Spielfeld_width)))
-        self.x = 0+feld_obj.x
-        self.y = random.randint(0, feld_obj.Spielfeld_height-int(0.0375*feld_obj.Spielfeld_width))
         self.schaden=1
+        self.x = 0+feld_obj.x
+        if random.randint(0,1)==1: #entscheidet von welcher Seite der Gegner kommt
+            self.x+=feld_obj.Spielfeld_width-int(0.0375*feld_obj.Spielfeld_width)
+        self.y = random.randint(0, feld_obj.Spielfeld_height-int(0.0375*feld_obj.Spielfeld_width))
+        self.side=0 #Spielfeld_1
+        if feld_obj.side==1:
+            self.side=1 #Spielfeld_2
 
     def Bewegen(self):
         print("Bewegen")
@@ -43,9 +48,7 @@ class Gegner:
     
     
     
-#Gehören zur Steuerung:
-    #if(Leben <= 0):
-        #Gui.lost_text()
+
 
     
     
