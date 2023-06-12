eca4aefc07c631f0452722d7a4753500808fb74d
MostafaGanji <88392156+MostafaGanji@users.noreply.github.com>
2023-06-12 14:50:26 +0200

    Update gegner.py

--- gegner.py
+++ gegner.py
@@ -8,9 +8,7 @@ pygame.init()
 class Gegner():
     
 
-    def gegner(self, feld_obj,x ,y):
-        self.x = x
-        self.y = y
+    def __init__(self, feld_obj):
 
 
         self.leben = 1
@@ -22,29 +20,29 @@ class Gegner():
         self.gegner_IMG = pygame.transform.scale(self.gegner_IMG, (self.size_gegner, self.size_gegner))
 
         #bestimmt von wo bis wo in x ache der Gegener kommen darf
-        self.gegner_startx = random.randrange(0,feld_obj.Spielfeld_width)
-        self.gegner_starty = -500
+        self.x = random.randrange(0,feld_obj.Spielfeld_width)
+        self.y = -500
         self.gegner_speed = 8
 
 
         #wird aufgerufen...(muss in steuerung)
-        self.gegner(self.gegner_startx,self.gegner_starty,self.x,self.y)
-        self.gegner_starty += self.gegner_speed 
+       
+        self.y += self.gegner_speed 
 
 
-        if self.gegner_starty > feld_obj.Spielfeld_height:
+        if self.y > feld_obj.Spielfeld_height:
             pass #den code von asteroiden, dass die gegnern sich annähren muss hier geschrieben werden
-            self.gegner_startx = random.randrange(0,feld_obj.Spielfeld_width)
+            self.x = random.randrange(0,feld_obj.Spielfeld_width)
 
         #mit self.x ist der x koordinate von spieler gemeint
-        if x > self.gegner_startx and x < self.gegner_startx + self.size_gegner or x + self.size > self.gegner_startx or x + self.size < self.gegner_startx + self.size_gegner:
+        if x > self.x and x < self.x + self.size_gegner or x + self.size > self.x or x + self.size < self.x + self.size_gegner:
             self.leben = self.leben - 1
             if self.leben == 0:
                 self.score = 1
 
 
         
-
+        self.side = feld_obj.side
 
 
 
