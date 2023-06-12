8a0d71c8e97a520d5e00e485fbe1568421373fdb
MostafaGanji <88392156+MostafaGanji@users.noreply.github.com>
2023-06-12 23:07:50 +0200

    Update gegner.py

--- gegner.py
+++ gegner.py
@@ -20,15 +20,15 @@ class Gegner():
         self.gegner_img = pygame.transform.scale(self.gegner_img, (self.size, self.size))
         self.aktuelles_bild=self.gegner_img
         #bestimmt von wo bis wo in x ache der Gegener kommen darf
-        self.x = random.randrange(0,feld_obj.Spielfeld_width)
+        self.x = random.randrange(feld_obj.x,feld_obj.x+feld_obj.Spielfeld_width)
         self.y = -500
         self.gegner_speed = 8
         self.mittelpunkt=[int(self.x+(34/64)*self.size), int(self.y+(44/64)*self.size)]
 
         #wird aufgerufen...(muss in steuerung)
        
-        self.y += self.gegner_speed 
-
+        #self.y += self.gegner_speed 
+        self.aktueller_richtungsvektor = [0,0]
 
         if self.y > feld_obj.Spielfeld_height:
             pass #den code von asteroiden, dass die gegnern sich annähren muss hier geschrieben werden
