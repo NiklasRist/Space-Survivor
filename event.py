98cd7f1e9c220611bb51a0c64591e12540008516
MostafaGanji <88392156+MostafaGanji@users.noreply.github.com>
2023-06-16 07:24:38 +0000

    event mujsss weiter gemacht werden

--- event.py
+++ event.py
@@ -7,3 +7,15 @@ class event:
     
     def ausfuehren(self):
         pass
+
+class event_1(event):
+    "Shwarzes loch wird erscheinen und den Raum Schiff zu sich anziehen"
+
+class event_2(event):
+    "Gegner wird für ein paar sekunden doppelt so viel schießen"
+
+class event_3(event):
+    "die richtungen werden vertaucht"
+
+class event_4(event):
+    "ist die "
