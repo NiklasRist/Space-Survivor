4eab594b35f5270cee75386fd4fa0b791c3dd657
MostafaGanji <mmostafaganji.mg@gmail.com>
2023-06-18 18:00:11 +0200

    2 events wurde aktiviert

--- event.py
+++ event.py
@@ -1,5 +1,7 @@
 import pygame
 from taste import verwalter
+import time
+import math
 class event:
     def __init__(self):
         pass
@@ -10,24 +12,99 @@ class event:
     def ausfuehren(self, spieler):
         pass
 
-class event_1(event):
+
+loch_erschinung = False
+loch_erschinung_start = None
+loch_erscheinung_dauer = 5
+class schwartzesloch(event):
     "Shwarzes loch wird erscheinen und das Raum Schiff zu sich anziehen"
+    def __init__(self,side):
+        self.side = side
+        self.x = 100
+        self.y = 200
+
+    def loch_erstellung(self):
+        loch_image = pygame.image.load(r'.\images\eclipse-2.png')
+        loch_rect = loch_image.get_rect()
+        loch_rect.center = (400, 200)
+
+    def loch_event(self):
+        global loch_erschinung
+        print("Hello das loch ist da")
+        loch_erschinung = True
+        loch_erschinung_start = time.time()
+
+    def berechne_vektor(self, obj_1, obj_2):
+        '''Berechnet einen Richtungsvektor aus den Koordinaten zweier Objekte'''
+        if obj_1!=obj_2:
+            return obj_2.mittelpunkt[0]-obj_1.mittelpunkt[0], obj_2.mittelpunkt[1]-obj_1.mittelpunkt[1]
+        else:
+            return obj_1.aktueller_richtungsvektor[0]-obj_1.mittelpunkt[0], obj_1.aktueller_richtungsvektor[1]-obj_1.mittelpunkt[1]  
+    def berechne_abstand(self, x, y):
+        '''Berechnet den Betrag(/Abstand) eines Vektors'''
+        return math.sqrt(x**2+y**2)       
+    def berechne_einheitsvektor(self, x, y, abstand):
+        '''Berechnet aus einem Richtungsvektor und Abstand einen Einheitsvektor'''
+        if abstand==0:
+            return 0,0
+        else:
+            return x/abstand, y/abstand  
+
+
+
+    def move_spieler_zum_loch(self, spieler_obj, spieler_obj_2,spieler_polygon,spieler_polygon_2):
+        '''Bewegt die Asteroiden und ihre Polygone um einen Einheitsvektor auf den Spieler zu.'''
+        #Abstandsberechnung Gegner Spieler (Vektorrechnung)
+        
+        if self.side == 0:
+            x,y = self.berechne_vektor(spieler_obj, self)
+            abstand=self.berechne_abstand(x,y)
+            if abstand>=1:
+                x_change,y_change=self.berechne_einheitsvektor(x,y,abstand)
+                spieler_obj.x+=x_change
+                spieler_obj.y+=y_change
+                spieler_obj.mittelpunkt= spieler_polygon.mittelpunkt
+                spieler_polygon.move_polygon([x_change, y_change]) 
+
+        if self.side==1:
+            x,y = self.berechne_vektor(spieler_obj_2, self)
+            abstand=self.berechne_abstand(x,y)
+            if abstand>=1:
+                x_change,y_change=self.berechne_einheitsvektor(x,y,abstand)
+                spieler_obj_2.x+=x_change
+                spieler_obj_2.y+=y_change
+                spieler_obj_2.mittelpunkt= spieler_polygon_2.mittelpunkt
+                spieler_polygon_2.move_polygon([x_change, y_change]) 
+
+
+
+
+
+
+
+
+
+
+    #if der spieler den event aktiviert:
+    # loch_event        
+
 
 class event_2(event):
-    "Gegner wird für ein paar sekunden doppelt so viel schießen"
+    "h"
+
 
 class verwirrung(event):
     "die richtungen werden vertaucht"
-    def key_änderung(steurung_obj):
+    def key_änderung(steuerung_obj):
         steuerung_obj.spieler_1_rechts = pygame.K_LEFT
         steuerung_obj.spieler_1_links = pygame.K_RIGHT
-        steuerung_obj.self.spieler_1_oben = pygame.K_DOWN
-        steuerung_obj.self.spieler_1_unten = pygame.K_UP
+        steuerung_obj.spieler_1_oben = pygame.K_DOWN
+        steuerung_obj.spieler_1_unten = pygame.K_UP
 
-        steuerung_obj.self.spieler_2_rechts = pygame.K_a
-        steuerung_obj.self.spieler_2_links = pygame.K_d
-        steuerung_obj.self.spieler_2_oben = pygame.K_s
-        steuerung_obj.self.spieler_2_unten = pygame.K_w 
+        steuerung_obj.spieler_2_rechts = pygame.K_a
+        steuerung_obj.spieler_2_links = pygame.K_d
+        steuerung_obj.spieler_2_oben = pygame.K_s
+        steuerung_obj.spieler_2_unten = pygame.K_w 
 
 
 class event_4(event):
